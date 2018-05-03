5f24c7af9f6af5a4d2d016ffabb9f9efef66f7eb
Shawn <ssaffarshargh@appacademy.io>
2018-05-03 14:45:47 -0700

    Fixing order of context/event

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = (handler) => {
   return async (event, context, callback) => {
     try {
-      const results = await handler(context);
+      const results = await handler(event, context);
       callback(null, results);
     } catch (error) {
       callback(error);
